d1ee6c4af78029e1fbe915cdbf49ed8b2526b7ed
Alexandre Roque <alexandreroque1@hotmail.com>
2022-04-24 18:42:13 -0300

    Added react-redux/reduxjs/toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import './index.tsx'
 import App from '.'
 import { QueryClientProvider } from 'react-query'
 import { queryClient } from './services/queryClient'
+import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={ queryClient } >
-      <App />
+      <Provider store={ store }>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
